0fdbb048ff204014aed3029ac0ee191ca3b9b136
zhanxinzhou <775297024@qq.com>
2021-02-22 18:02:54 +0800

    本地工程添加到git文件

---
+++ Git-test/src/com/zxz/git/FirstClass.java
@@ -0,0 +1,18 @@
+package com.zxz.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello,git Project!!!");
+        System.out.println("hello,git Project!!!");
+        System.out.println("hello,git Project!!!");
+        System.out.println("hello,git Project!!!");
+        System.out.println("hello,git Project!!!");
+        System.out.println("hello,git Project!!!");
+        System.out.println("hello,git Project!!!");
+        System.out.println("hello,git Project!!!");
+        System.out.println("hello,git Project!!!");
+
+
+
+    }
+}
